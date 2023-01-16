b9f524cb698659782a67cb3f0edc59c6e125ad48
Alex <awarrington0895@gmail.com>
2023-01-16 22:12:23 +0000

    Add eslint

--- functions/sendmessage/index.ts
+++ functions/sendmessage/index.ts
@@ -1,71 +1,79 @@
 import {
-    ApiGatewayManagementApiClient,
-    GetConnectionCommand,
-    PostToConnectionCommand,
-    PostToConnectionCommandInput,
-  } from "@aws-sdk/client-apigatewaymanagementapi";
-import { DynamoDBClient, ScanCommand, ScanCommandInput, ScanCommandOutput } from "@aws-sdk/client-dynamodb";
-  import { APIGatewayEvent, Handler } from "aws-lambda";
-  
-  import { badRequest, ok, serverError } from "../shared";
+  ApiGatewayManagementApiClient,
+  PostToConnectionCommand,
+} from "@aws-sdk/client-apigatewaymanagementapi";
+import {
+  DynamoDBClient,
+  ScanCommand,
+  ScanCommandOutput,
+} from "@aws-sdk/client-dynamodb";
+import { APIGatewayEvent, Handler } from "aws-lambda";
 
-  const dynamoClient = new DynamoDBClient({ region: 'us-east-1' });
-  
-  export const handler: Handler = async (event: APIGatewayEvent) => {
-    const endpoint = `https://${event.requestContext.domainName}/${event.requestContext.stage}`;
-  
-    const apigw = new ApiGatewayManagementApiClient({
-      region: "us-east-1",
-      endpoint,
-    });
+import { badRequest, ok, serverError } from "../shared";
 
-    const message: string = JSON.parse(event.body || "{ message: ''}").message;
+const dynamoClient = new DynamoDBClient({ region: "us-east-1" });
 
-    if (message === '') {
-        const errorMessage = 'Body must contain a message attribute with a non-empty string';
-        try {
-            await apigw.send(new PostToConnectionCommand({
-                ConnectionId: event.requestContext.connectionId,
-                Data: Buffer.from(errorMessage)
-            }));
-        } catch(err) {
-            console.error(err);
-        }
-        
-        return badRequest(errorMessage);
-    }
+export const handler: Handler = async (event: APIGatewayEvent) => {
+  const endpoint = `https://${event.requestContext.domainName}/${event.requestContext.stage}`;
 
-    let connections: ScanCommandOutput;
+  const apigw = new ApiGatewayManagementApiClient({
+    region: "us-east-1",
+    endpoint,
+  });
 
+  const message: string = JSON.parse(event.body || "{ message: ''}").message;
+
+  if (message === "") {
+    const errorMessage =
+      "Body must contain a message attribute with a non-empty string";
     try {
-        connections = await dynamoClient.send(new ScanCommand({ TableName: process.env.table }));
+      await apigw.send(
+        new PostToConnectionCommand({
+          ConnectionId: event.requestContext.connectionId,
+          Data: Buffer.from(errorMessage),
+        })
+      );
     } catch (err) {
-        console.error('Could not fetch connections from DynamoDb: ', err);
-
-        return serverError('Failed to fetch connections');
+      console.error(err);
     }
 
-    const sendMessages = connections.Items?.map(async (item) => {
-        const connectionId = item['connectionId'].S;
+    return badRequest(errorMessage);
+  }
 
-        if (connectionId !== event.requestContext.connectionId) {
-            try {
-                await apigw.send(new PostToConnectionCommand({
-                    ConnectionId: connectionId,
-                    Data: Buffer.from(JSON.stringify(message))
-                }))
-            } catch (err) {
-                console.error('Unable to post message to connection: ', err);
-            }
-        }
-    });
+  let connections: ScanCommandOutput;
 
-    try {
-        await Promise.all(sendMessages!);
-    } catch (err) {
-        return serverError('Unable to send messages');
+  try {
+    connections = await dynamoClient.send(
+      new ScanCommand({ TableName: process.env.table })
+    );
+  } catch (err) {
+    console.error("Could not fetch connections from DynamoDb: ", err);
+
+    return serverError("Failed to fetch connections");
+  }
+
+  const sendMessages = connections.Items?.map(async (item) => {
+    const connectionId = item["connectionId"].S;
+
+    if (connectionId !== event.requestContext.connectionId) {
+      try {
+        await apigw.send(
+          new PostToConnectionCommand({
+            ConnectionId: connectionId,
+            Data: Buffer.from(JSON.stringify(message)),
+          })
+        );
+      } catch (err) {
+        console.error("Unable to post message to connection: ", err);
+      }
     }
+  });
 
-    return ok();
-  };
-  
+  try {
+    await Promise.all(sendMessages!);
+  } catch (err) {
+    return serverError("Unable to send messages");
+  }
+
+  return ok();
+};
