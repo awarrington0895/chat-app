15c5f9bd10d60e0cfbd4d3c22b2ce61a6322cba6
Alex <awarrington0895@gmail.com>
2023-01-16 17:44:32 +0000

    Initial connect lambda

---
+++ functions/connect/index.ts
@@ -0,0 +1,44 @@
+import { APIGatewayEvent, Context, Handler } from 'aws-lambda';
+import { DynamoDBClient, PutItemCommand, PutItemCommandInput } from '@aws-sdk/client-dynamodb';
+
+const client = new DynamoDBClient({ region: 'us-east-1' });
+
+const createResponse = (statusCode: number, message?: string) => ({
+    statusCode,
+    message
+});
+
+const badRequest = (message: string) => createResponse(400, message);
+
+const ok = (message?: string) => createResponse(200, message);
+
+const serverError = (message: string) => createResponse(500, message);
+
+export const handler: Handler = async (event: APIGatewayEvent, context: Context) => {
+    const connectionId = event.requestContext.connectionId;
+
+    if (connectionId == null) {
+        return badRequest('Must have a valid connectionId to create session');
+    }
+
+    const params: PutItemCommandInput = {
+        TableName: process.env.table,
+        Item: {
+            connectionId: {
+                S: connectionId
+            }
+        }
+    };
+
+    const createConnection = new PutItemCommand(params);
+
+    try {
+        await client.send(createConnection);    
+    } catch (err) {
+        console.error(err);
+
+        return serverError('Unable to create a session due to unknown server error');
+    }
+
+    return ok();
+};
--- lib/app-stack.ts
+++ lib/app-stack.ts
@@ -1,6 +1,9 @@
-import { Stack, StackProps, aws_dynamodb as dynamo } from "aws-cdk-lib";
+import { Stack, StackProps, aws_dynamodb as dynamo, aws_lambda as lambda } from "aws-cdk-lib";
 import { WebSocketApi, WebSocketStage } from "@aws-cdk/aws-apigatewayv2-alpha";
 import { Construct } from "constructs";
+import { NodejsFunction } from "aws-cdk-lib/aws-lambda-nodejs";
+import path = require("path");
+import { WebSocketLambdaIntegration } from "@aws-cdk/aws-apigatewayv2-integrations-alpha";
 
 export class AppStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
@@ -19,6 +22,19 @@ export class AppStack extends Stack {
       webSocketApi: chatApi,
       stageName: "production",
       autoDeploy: true,
+    });
+
+    const connectFn = new NodejsFunction(this, 'connectFn', {
+      runtime: lambda.Runtime.NODEJS_18_X,
+      entry: path.join(__dirname, `/../functions/connect/index.ts`),
+      handler: 'handler',
+      environment: {
+        table: connectionTable.tableName
+      }
+    });
+
+    chatApi.addRoute('$connect', {
+      integration: new WebSocketLambdaIntegration('ConnectIntegration', connectFn)
     });
   }
 }
