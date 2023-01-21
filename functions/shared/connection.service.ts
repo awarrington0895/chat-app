3cc82897b9542173e6a69e6095774fe829b3f23a
Alex <awarrington0895@gmail.com>
2023-01-21 03:56:36 +0000

    Add method for deleting connection

--- functions/shared/connection.service.ts
+++ functions/shared/connection.service.ts
@@ -1,6 +1,9 @@
 import {
+  DeleteItemCommand,
+  DeleteItemCommandInput,
   DynamoDBClient,
   PutItemCommand,
+  PutItemCommandInput,
 } from "@aws-sdk/client-dynamodb";
 import * as F from "fp-ts/function";
 import * as TE from "fp-ts/TaskEither";
@@ -12,7 +15,11 @@ type Config = {
 };
 
 export const createConnectionService = ({ dynamodb, tableName }: Config) => {
-  const setConnectionDetails = (connectionId: string) => new PutItemCommand({
+  const createPutItemCommand = (details: PutItemCommandInput) => new PutItemCommand(details);
+
+  const createDeleteItemCommand = (details: DeleteItemCommandInput) => new DeleteItemCommand(details);
+
+  const createConnectionDetails = (connectionId: string) => new PutItemCommand({
     TableName: tableName,
     Item: {
       connectionId: {
@@ -21,16 +28,35 @@ export const createConnectionService = ({ dynamodb, tableName }: Config) => {
     },
   });
 
+  const createDeletionDetails = (connectionId: string) => new DeleteItemCommand({
+    TableName: tableName,
+        Key: {
+            connectionId: {
+                S: connectionId
+            }
+        }
+  });
+
   const tryCreateConnection = (command: PutItemCommand) =>
     TE.tryCatch(
       () => dynamodb.send(command),
       () => serverError("Unable to create a session due to unknown server error")
     );
 
+  const tryDeleteConnection = (command: DeleteItemCommand) => TE.tryCatch(
+    () => dynamodb.send(command),
+    () => serverError('Unable to remove a session due to unknown server error')
+  );
+
   return {
     createConnection: F.flow(
-      setConnectionDetails,
+      createConnectionDetails,
       tryCreateConnection
     ),
+
+    deleteConnection: F.flow(
+      createDeletionDetails,
+      tryDeleteConnection
+    )
   };
 };
