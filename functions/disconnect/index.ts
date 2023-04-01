a9207a934d0605fb3b405e5451427140c959a6ad
Alex <awarrington0895@gmail.com>
2023-04-01 17:11:22 +0000

    Refactor disconnect fn to use functional style

--- functions/disconnect/index.ts
+++ functions/disconnect/index.ts
@@ -1,34 +1,66 @@
-import { APIGatewayEvent, Handler } from 'aws-lambda';
-import { DeleteItemCommand, DeleteItemCommandInput, DynamoDBClient } from '@aws-sdk/client-dynamodb';
-import { badRequest, ok, serverError } from '../shared';
+import {
+  DeleteItemCommand,
+  DeleteItemCommandInput,
+  DeleteItemCommandOutput,
+  DynamoDBClient,
+} from "@aws-sdk/client-dynamodb";
+import { APIGatewayEvent, Handler } from "aws-lambda";
+import * as TE from "fp-ts/TaskEither";
+import { pipe } from "fp-ts/function";
+import {
+  EventParser,
+  HandlerResponse,
+  badRequest,
+  ok,
+  serverError,
+} from "../shared";
 
-const client = new DynamoDBClient({ region: 'us-east-1' });
+const client = new DynamoDBClient({ region: "us-east-1" });
 
-export const handler: Handler = async (event: APIGatewayEvent) => {
-    const connectionId = event.requestContext.connectionId;
+export const handler: Handler = async (
+  event: APIGatewayEvent
+): Promise<HandlerResponse> =>
+  pipe(
+    TE.Do,
+    TE.bind("connectionId", () => EventParser.connectionId(event)),
+    TE.bind("table", () => parseTableName(process.env.table)),
+    TE.map(deleteConnectionInput),
+    TE.chain((params) => pipe(client, deleteConnection(params))),
+    TE.map(() => ok("Disconnected")),
+    TE.toUnion
+  )();
 
-    if (connectionId == null) {
-        return badRequest('Must have a valid connectionId to disconnect a session');
-    }
-
-    const params: DeleteItemCommandInput = {
-        TableName: process.env.table,
-        Key: {
-            connectionId: {
-                S: connectionId
-            }
-        }
-    };
-
-    const deleteConnection = new DeleteItemCommand(params);
-
-    try {
-        await client.send(deleteConnection);    
-    } catch (err) {
-        console.error(err);
-
-        return serverError('Unable to remove a session due to unknown server error');
-    }
-
-    return ok('Disconnected');
+type Context = {
+  readonly connectionId: string;
+  readonly table: string;
 };
+
+const deleteConnectionInput = (ctx: Context): DeleteItemCommandInput => ({
+  TableName: ctx.table,
+  Key: {
+    connectionId: {
+      S: ctx.connectionId,
+    },
+  },
+});
+
+const deleteConnection =
+  (params: DeleteItemCommandInput) =>
+  (
+    client: DynamoDBClient
+  ): TE.TaskEither<HandlerResponse, DeleteItemCommandOutput> =>
+    TE.tryCatch(
+      () => client.send(new DeleteItemCommand(params)),
+      () =>
+        serverError("Unable to remove a session due to unknown server error")
+    );
+
+const parseTableName = (
+  tableName: string | undefined
+): TE.TaskEither<HandlerResponse, string> =>
+  pipe(
+    tableName,
+    TE.fromNullable(
+      badRequest("Must have a valid table name to disconnect a session")
+    )
+  );
