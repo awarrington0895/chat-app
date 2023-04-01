074c751f99571b703d386abd6576d78445252a90
Alex <awarrington0895@gmail.com>
2023-04-01 10:45:07 -0500

    Refactor connect to use functional style (#4)

--- functions/connect/index.ts
+++ functions/connect/index.ts
@@ -1,40 +1,80 @@
-import { APIGatewayEvent, Handler } from 'aws-lambda';
-import { DynamoDBClient, PutItemCommand, PutItemCommandInput } from '@aws-sdk/client-dynamodb';
-import { badRequest, ok, serverError } from '../shared';
+import { APIGatewayEvent, Handler } from "aws-lambda";
+import {
+  DynamoDBClient,
+  PutItemCommand,
+  PutItemCommandInput,
+} from "@aws-sdk/client-dynamodb";
+import { HandlerResponse, badRequest, ok, serverError } from "../shared";
+import { pipe } from "fp-ts/function";
+import * as TE from "fp-ts/TaskEither";
 
-const client = new DynamoDBClient({ region: 'us-east-1' });
+const client = new DynamoDBClient({ region: "us-east-1" });
 
-export const handler: Handler = async (event: APIGatewayEvent) => {
-    const connectionId = event.requestContext.connectionId;
+export const handler: Handler = async (
+  event: APIGatewayEvent
+): Promise<HandlerResponse> =>
+  pipe(
+    event,
+    TE.right,
+    TE.bindTo("event"),
+    TE.bind("connectionId", ({ event }) => parseConnectionId(event)),
+    TE.bind("principalId", ({ event }) => parsePrincipalId(event)),
+    TE.bind("groups", ({ event }) => parseGroups(event)),
+    TE.map(createConnectionParams),
+    TE.chain(createConnection),
+    TE.map(() => ok("Connected")),
+    TE.toUnion
+  )();
 
-    if (connectionId == null) {
-        return badRequest('Must have a valid connectionId to create session');
-    }
-
-    const params: PutItemCommandInput = {
-        TableName: process.env.table,
-        Item: {
-            connectionId: {
-                S: connectionId
-            },
-            personId: {
-                S: event.requestContext.authorizer?.principalId
-            },
-            groups: {
-                S: event.requestContext.authorizer?.groups
-            }
-        }
-    };
-
-    const createConnection = new PutItemCommand(params);
-
-    try {
-        await client.send(createConnection);    
-    } catch (err) {
-        console.error(err);
-
-        return serverError('Unable to create a session due to unknown server error');
-    }
-
-    return ok('Connected');
+type ConnectionInput = {
+  connectionId: string;
+  principalId: string;
+  groups: string;
 };
+
+const parsePrincipalId = (event: APIGatewayEvent) =>
+  pipe(
+    event.requestContext.authorizer?.principalId,
+    parseEventKey("principalId")
+  );
+
+const parseGroups = (event: APIGatewayEvent) =>
+  pipe(event.requestContext.authorizer?.groups, parseEventKey("groups"));
+
+const parseConnectionId = (event: APIGatewayEvent) =>
+  pipe(event.requestContext.connectionId, parseEventKey("connectionId"));
+
+const parseEventKey =
+  (keyName: string) =>
+  (key: string | undefined): TE.TaskEither<HandlerResponse, string> =>
+    pipe(
+      key,
+      TE.fromNullable(badRequest(`Event must have a valid ${keyName}`))
+    );
+
+const createConnectionParams = ({
+  connectionId,
+  principalId,
+  groups,
+}: ConnectionInput) => {
+  return {
+    TableName: process.env.table,
+    Item: {
+      connectionId: {
+        S: connectionId,
+      },
+      personId: {
+        S: principalId,
+      },
+      groups: {
+        S: groups,
+      },
+    },
+  };
+};
+
+const createConnection = (params: PutItemCommandInput) =>
+  TE.tryCatch(
+    () => client.send(new PutItemCommand(params)),
+    () => serverError("Unable to create a session due to unknown server error")
+  );
