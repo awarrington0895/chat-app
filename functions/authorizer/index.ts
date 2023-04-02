fb62b1453322a5f4ae77e5e85ae56c5e3b5672f8
Alex <awarrington0895@gmail.com>
2023-04-02 15:16:10 +0000

    Switch to somewhat more functional style with Optional

--- functions/authorizer/index.ts
+++ functions/authorizer/index.ts
@@ -2,44 +2,71 @@ import {
   APIGatewayAuthorizerResult,
   APIGatewayRequestAuthorizerEvent,
 } from "aws-lambda";
+import { pipe } from "fp-ts/function";
 import * as jwt from "jsonwebtoken";
+import * as O from 'fp-ts/Option';
+import * as TE from 'fp-ts/TaskEither';
+
+export const handler = async (
+  event: APIGatewayRequestAuthorizerEvent
+): Promise<APIGatewayAuthorizerResult> => {
+  const payload = decode(event?.queryStringParameters?.access_token);
+
+  if (O.isNone(payload)) {
+    return generateDeny("anonymous", event.methodArn);
+  }
+
+  const personId = pipe(
+    payload,
+    O.map(payload => payload.personId),
+  );
+
+  if (O.isNone(personId)) {
+    return generateDeny("anonymous", event.methodArn);
+  }
+
+  const inGroup = pipe(
+    payload,
+    O.map(payload => payload.groups),
+    O.map(groups => userInAGroup(groups)),
+    O.filter(inGroup => inGroup)
+  );
+
+  if (O.isNone(inGroup)) {
+    return generateDeny(personId.value, event.methodArn);
+  }
+
+  return pipe(
+    payload,
+    O.map(payload => {
+      return {
+        ...generateAllow(payload.personId, event.methodArn),
+        context: {
+          groups: JSON.stringify(payload.groups),
+        },
+      };
+    }),
+    TE.fromOption(() => generateDeny(personId.value, event.methodArn)),
+    TE.toUnion
+  )();
+};
 
 type CustomJwtPayload = {
   personId: string;
   groups: string[];
 };
 
-export const handler = async (
-  event: APIGatewayRequestAuthorizerEvent
-): Promise<APIGatewayAuthorizerResult> => {
-  if (!event?.queryStringParameters?.access_token) {
-    return generateDeny("anonymous", event.methodArn);
-  }
+function decode(token: string | undefined): O.Option<CustomJwtPayload> {
+  return pipe(
+    O.fromNullable(token),
+    O.map(jwt.decode),
+    O.map(decoded => decoded as CustomJwtPayload)
+  );
+}
 
-  const decoded = jwt.decode(
-    event.queryStringParameters["access_token"]
-  ) as CustomJwtPayload;
-
-  console.log("Decoded: ", decoded);
-
-  if (!decoded?.personId) {
-    return generateDeny("anonymous", event.methodArn);
-  }
-
-  const userInAGroup = decoded.groups.some(group => group === 'chat.admin' || group === 'chat.user');
-
-  if (!userInAGroup) {
-    return generateDeny(decoded.personId, event.methodArn);
-  }
-
-
-  return {
-    ...generateAllow(decoded.personId, event.methodArn),
-    context: {
-      groups: JSON.stringify(decoded.groups),
-    },
-  };
-};
+function userInAGroup(groups: string[]): boolean {
+  return groups.some(group => group === 'chat.admin' || group === 'chat.user');
+}
 
 function generatePolicy(
   principalId: string,
