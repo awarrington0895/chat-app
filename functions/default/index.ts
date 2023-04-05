0c865eb1c21306d4199a353e5a81f19dceec54f6
Alex <awarrington0895@gmail.com>
2023-04-05 17:40:15 -0500

    Refactor/default route to functional style (#7)
    * Refactor default route to use functional style
    
    * Use the right import path

--- functions/default/index.ts
+++ functions/default/index.ts
@@ -1,50 +1,89 @@
 import {
   ApiGatewayManagementApiClient,
   GetConnectionCommand,
+  GetConnectionCommandOutput,
   PostToConnectionCommand,
   PostToConnectionCommandInput,
 } from "@aws-sdk/client-apigatewaymanagementapi";
 import { APIGatewayEvent, Handler } from "aws-lambda";
 
-import { ok, serverError } from "../shared";
+import { EventParser, ok, serverError } from "../shared";
+import { pipe } from "fp-ts/function";
+import * as TE from "fp-ts/TaskEither";
+import * as O from 'fp-ts/Option';
+import * as J from 'fp-ts/Json';
+import * as E from 'fp-ts/Either';
+import { Task } from "fp-ts/Task";
 
 export const handler: Handler = async (event: APIGatewayEvent) => {
-  const endpoint = `https://${event.requestContext.domainName}/${event.requestContext.stage}`;
-
-  const client = new ApiGatewayManagementApiClient({
-    region: "us-east-1",
-    endpoint,
-  });
-
-  const connectionId = event.requestContext.connectionId;
-
-  let connectionInfo;
-
-  try {
-    connectionInfo = await client.send(
-      new GetConnectionCommand({
-        ConnectionId: connectionId,
-      })
-    );
-  } catch (err) {
-    connectionInfo = connectionId;
-  }
-
-  const params: PostToConnectionCommandInput = {
-    ConnectionId: connectionId,
-    Data: Buffer.from(
-      "Use the sendmessage route to send a message. Your info: " +
-        JSON.stringify(connectionInfo)
+  return pipe(
+    event,
+    createGatewayClient,
+    TE.fromOption(() => serverError("Could not create gateway client")),
+    TE.bindTo("client"),
+    TE.bind("connectionId", () => EventParser.connectionId(event)),
+    TE.bind("connectionInfo", ({ client, connectionId }) =>
+      TE.fromTask(additionalConnectionInfo(client, connectionId))
     ),
+    TE.bind('postParams', ({ connectionId, connectionInfo }) => getPostParms(connectionId, connectionInfo)),
+    TE.chain(({ client, postParams }) => tryPostMessage(client, postParams)),
+    TE.map(() => ok()),
+    TE.toUnion
+  );
+};
+
+function getPostParms(connectionId: string, connectionInfo: string | GetConnectionCommandOutput) {
+  return pipe(
+    J.stringify(connectionInfo),
+    E.map(info => ({
+      ConnectionId: connectionId,
+      Data: Buffer.from(
+        "Use the sendmessage route to send a message. Your info: " +
+          info
+      ),
+    })),
+    E.mapLeft(() => serverError("Failed to parse connection info")),
+    TE.fromEither
+  )
+
+}
+
+function additionalConnectionInfo(
+  client: ApiGatewayManagementApiClient,
+  connectionId: string
+): Task<string | GetConnectionCommandOutput> {
+  const result = async () => {
+    try {
+      return await client.send(
+        new GetConnectionCommand({ ConnectionId: connectionId })
+      );
+    } catch (err) {
+      return connectionId;
+    }
   };
 
-  try {
-    await client.send(new PostToConnectionCommand(params));
-  } catch (err) {
-    console.error(err);
+  return result;
+}
 
-    return serverError("Error occurred during post to WS connection");
-  }
+function createGatewayClient(event: APIGatewayEvent): O.Option<ApiGatewayManagementApiClient> {
+  return pipe(
+    O.Do,
+    O.apS('domainName', O.fromNullable(event.requestContext.domainName)),
+    O.apS('stage', O.fromNullable(event.requestContext.stage)),
+    O.map(({ domainName, stage }) => `https://${domainName}/${stage}`),
+    O.map(endpoint => new ApiGatewayManagementApiClient({
+      region: "us-east-1",
+      endpoint,
+    })),
+  );
+}
 
-  return ok();
-};
+function tryPostMessage(
+  client: ApiGatewayManagementApiClient,
+  params: PostToConnectionCommandInput
+) {
+  return TE.tryCatch(
+    () => client.send(new PostToConnectionCommand(params)),
+    () => serverError("Error occurred during post to WS connection")
+  );
+}
